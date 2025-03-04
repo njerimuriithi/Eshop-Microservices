866fd9caaee26791e9214b517218fa44521f6390
Njeri Muriithi <cnjeri388@gmail.com>
2025-03-04 10:46:54 +0300

    Added the GetProductByIdImplemetation

---
+++ Services/Catalog/CatalogAPI/Exceptions/productNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace CatalogAPI.Exceptions
+{
+    public class productNotFoundException:Exception
+    {
+        public productNotFoundException() : base("Product not Found!")
+        {
+
+        }
+    }
+}
--- Services/Catalog/CatalogAPI/Products/GetProductById/GetProductByIdHandler.cs
+++ Services/Catalog/CatalogAPI/Products/GetProductById/GetProductByIdHandler.cs
@@ -1,6 +1,18 @@
-﻿namespace CatalogAPI.Products.GetProductById
+﻿
+
+namespace CatalogAPI.Products.GetProductById
 {
-    public class GetProductByIdHandler
+    public record GetProductByIdQuery(Guid id):IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+    internal class GetProductByIdQueryHandler(IDocumentSession session,ILogger<GetProductByIdQueryHandler>logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
     {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductbyIdQueryHandler{@query}");
+             var product = await session.LoadAsync<Product>(query.id, cancellationToken); 
+            if(product is  null) { throw new productNotFoundException();}
+              
+            return new GetProductByIdResult(product);
+        }
     }
 }
