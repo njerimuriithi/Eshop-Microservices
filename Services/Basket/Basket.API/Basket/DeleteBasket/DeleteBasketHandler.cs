9e9c2a2fca706c3c47f959a7f9bc1b98395e90b2
Njeri Muriithi <cnjeri388@gmail.com>
2025-04-24 11:40:00 +0300

    Delete and Store Basket Implementations

--- Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,6 +1,21 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿
+namespace Basket.API.Basket.DeleteBasket
 {
-    public class DeleteBasketHandler
+    public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+    public record DeleteBasketResult(bool isSuccess);
+
+    public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
     {
+        public DeleteBasketCommandValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is Requred");
+        }
+    }
+    public class DeleteBasketCommandHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+    {
+        public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+        {
+            return new DeleteBasketResult(true);
+        }
     }
 }
--- Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,24 @@
-﻿namespace Basket.API.Basket.StoreBasket
+﻿
+
+namespace Basket.API.Basket.StoreBasket
 {
-    public class StoreBasketHandler
+     public record StoreBasketCommand(ShoppingCart Cart):ICommand<StoreBasketResult>;
+     public record StoreBasketResult(string UserName);
+
+     public class StoreBasketCommandValidator:AbstractValidator<StoreBasketCommand>
     {
+       public StoreBasketCommandValidator()
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Cart Can not be null");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is Requred");
+        }
+    }
+    public class StoreBasketHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+        {
+            ShoppingCart cart = command.Cart;   
+            return new StoreBasketResult("swn");
+        }
     }
 }
