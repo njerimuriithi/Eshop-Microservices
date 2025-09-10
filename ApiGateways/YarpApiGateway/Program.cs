ee0e1222f89713a23051b5a1c5920500a36d4825
carolyne <carolynenjeri@cim-pool.ch>
2025-09-10 12:34:21 +0300

    Yarp API GateWay Routing Pattern Implementation

---
+++ ApiGateways/YarpApiGateway/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+//addratelimiter
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
+var app = builder.Build();
+
+//configure the HTTP request pipeline.
+app.UseRateLimiter();
+app.MapReverseProxy();
+
+app.Run();
